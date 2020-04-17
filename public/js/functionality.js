add43a3c7b95aaa9c301a22ada30e2c27d592c19
N1hiI <spi.nihil@yahoo.com>
2020-04-17 21:20:19 +0300

    dummy change

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -11,7 +11,7 @@ console.log("print from js file")
 
 const weatherForm = document.querySelector('form')
 const ssearch = document.querySelector('input')
-const msg2 = document.querySelector('#msg2')
+const mesg2 = document.querySelector('#msg2')
 
 
 
@@ -22,7 +22,7 @@ weatherForm.addEventListener('submit',(e)=>{
     fetch('http://127.0.0.1:3000/weather?location='+location).then((response)=>{
     response.json().then((data)=>{
         //console.log(data)
-        msg2.textContent = 'Am primit locatia sau ceva'+data.location
+        mesg2.textContent = 'Am primit locatia sau ceva'+data.location
 
     })
 })
